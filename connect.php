0bd43cb08dfa8ff3cc2ccb49d8eacac6106b4e72
Worawan Khakho <worawan-kh@rmutp.ac.th>
2022-02-19 16:15:28 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+  //ตั้งค่าการเชื่อมต่อฐานข้อมูล
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "dbproject";
+	
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+  // Check connection
+  if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+  }
+  //กำหนด charset ให้เป็น utf8 เพื่อรองรับภาษาไทย
+  mysqli_set_charset($conn,'UTF8');
+?>
