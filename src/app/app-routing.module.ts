20b9de24ba339cfbe89d1557e342a11aea008fa2
Worawan Khakho <worawan-kh@rmutp.ac.th>
2022-02-19 23:57:17 +0700

    fix code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'news', component: NewsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
