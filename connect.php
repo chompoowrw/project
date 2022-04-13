9ea54d4c2f589e2d54f14c1cea1bf7a62cf93a78
songk464 <songk464@gnsof.postbox.in.th>
2022-04-14 00:47:12 +0700

    fix code

--- connect.php
+++ connect.php
@@ -1,18 +1,27 @@
 <?php
-  //ตั้งค่าการเชื่อมต่อฐานข้อมูล
+# Is this the production server or not?
+define("PRODUCTION", $_SERVER["SERVER_NAME"] == "web.rmutp.ac.th");
+# ตั้งค่าการเชื่อมต่อฐานข้อมูล
+if (PRODUCTION) {
+  # Production
+  $servername = "localhost";
+  $username = "luxurybyfon";
+  $password = "mj95c2gx";
+  $dbname = "luxurybyfon";
+} else {
+  # Development
   $servername = "localhost";
   $username = "root";
   $password = "";
   $dbname = "ngproject";
-	
-  // Create connection
-  $conn = new mysqli($servername, $username, $password, $dbname);
-  // Check connection
-  if ($conn->connect_error) {
-	  die("Connection failed: " . $conn->connect_error);
-  } else {
-    echo "Connection to server";
-  }
-  //กำหนด charset ให้เป็น utf8 เพื่อรองรับภาษาไทย
-  mysqli_set_charset($conn,'UTF8');
+}
+
+# Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+# Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+# กำหนด charset ให้เป็น utf8 เพื่อรองรับภาษาไทย
+mysqli_set_charset($conn, "UTF8");
 ?>
