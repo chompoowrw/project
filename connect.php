b331eeaa3890e22d165485ee40ae596223884327
songk464 <songk464@gnsof.postbox.in.th>
2022-04-13 22:20:57 +0700

    fix code

--- connect.php
+++ connect.php
@@ -9,7 +9,9 @@
   $conn = new mysqli($servername, $username, $password, $dbname);
   // Check connection
   if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+	  die("Connection failed: " . $conn->connect_error);
+  } else {
+    echo "Connection to server";
   }
   //กำหนด charset ให้เป็น utf8 เพื่อรองรับภาษาไทย
   mysqli_set_charset($conn,'UTF8');
